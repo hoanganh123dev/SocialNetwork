f636a9efca7a48836517c4eb4d06a05667c401bc
Hoàng Anh <112646804+hoanganh123dev@users.noreply.github.com>
2023-12-16 01:00:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/socialnetwork/RegisterActivity.java
@@ -0,0 +1,23 @@
+package com.example.socialnetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText UserEmail, UserPassword, UserConfirmPassword;
+    private Button CreateAccountButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+
+        UserEmail = (EditText) findViewById(R.id.register_email);
+        UserPassword = (EditText) findViewById(R.id.register_password);
+        UserConfirmPassword = (EditText) findViewById(R.id.register_confirm_password);
+        CreateAccountButton = (Button) findViewById(R.id.register_create_account);
+    }
+}
